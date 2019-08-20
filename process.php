8e9a14238af7af23f58cf161dcf9099af824b7ef
Jatingupta-2 <38355753+Jatingupta-2@users.noreply.github.com>
2019-08-20 20:28:43 +0530

    ddwd

---
+++ process.php
@@ -0,0 +1,29 @@
+<?php
+$visitor_email = $_POST['email'];
+$visitor_name = $_POST['name'];
+$visitor_message = $_POST['message'];
+$visitor_phone = $_POST['phone'];
+
+
+
+ $to = 'jating72@gmail.com';
+ $subject =  'Visitor '. $visitor_name .' with email Id "'. $visitor_email. '"and Phone no.'.$visitor_phone.' wants to contact you !';
+ $message = 'Message:' .$visitor_message;
+ $headers = "Reply-To: The Sender <jating72@gmail.com\r\n"; 
+  $headers = "Return-Path: The Sender <jating72@gmail.com\r\n"; 
+  $headers = "From: The Sender <jating72@gmail.com\r\n"; 
+ $headers = "From: jating72@gmail.com\n";
+ $headers = "MIME-Version: 1.0\n";
+ $headers = "Content-type: text/html; charset=iso-8859-1\n";
+  $headers = "Organization: Sender Organization\r\n";
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers = "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers = "X-Priority: 3\r\n";
+  $headers = "X-Mailer: PHP". phpversion() ."\r\n";
+ $submit_success = mail($to,$subject,$message,$headers);
+ if ($submit_success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+?>
